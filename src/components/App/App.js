17edb1955bf1a5fc977b470ca442421d9b21942f
Sara Ebbers <slebbers@gmail.com>
2019-02-13 14:01:42 -0700

    Add Router, Add shell of routed components, rename components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link, NavLink } from 'react-router-dom';
+import '../../index.scss';
+import Home from '../../containers/Home/Home';
+import Display from '../../components/Display/Display';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="main">
-        <h1>My Health Navigator</h1>      
-      </div>
+      <section className="main-container">
+        <header className="header-container">
+          <Link to='/' className='link-header'>
+            <h1>My Health Navigator</h1>  
+          </Link>
+        </header>
+        <nav className='nav-container'>
+          <NavLink to='/profile' className='navigation'>Profile</NavLink>
+          <NavLink to='/appointments' className='navigation'>Appointments</NavLink>
+          <NavLink to='/providers' className='navigation'>Providers</NavLink>
+          <NavLink to='/insurance' className='insurance'>Insurance</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/profile' render={() => <Display type="profile" />} />
+          <Route exact path='/appointments' render={() => <Display type="appointments" />} />
+          <Route exact path='/providers' render={() => <Display type="providers" />} />
+          <Route exact path='/insurance' render={() => <Display type="insurance" />} />   
+          <Route render={() => (
+            <div className='redirect'>
+              <Link to='/' className='link-redirect'>
+                <h2>Can't find that page</h2>
+                <button className='home'> Click to Return Home</button>
+              </Link>
+            </div>
+            )}  />     
+        </Switch>
+      </section>
     );
   }
 }
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../index.scss';
+import PropTypes from 'prop-types';
+
+
+class Home extends Component {
+
+  render() {
+    return(
+      <div>This is the Home Component</div>
+    )
+  }
+}
+
+export default Home;
