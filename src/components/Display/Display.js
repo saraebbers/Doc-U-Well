c511ccb71e4090384c6c9f41658a551937719fd4
Sara Ebbers <slebbers@gmail.com>
2019-02-13 16:47:09 -0700

    Refactor switch

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -10,7 +10,7 @@ class Display extends Component {
     let btnName
     let information
 
-    console.log(this.props.type)
+    console.log(this.props)
 
     switch (this.props.type) {
       case 'profile' : 
@@ -18,31 +18,47 @@ class Display extends Component {
         // this.props.fetchProfile(url)
         btnName = 'Update Profile'
         information = 'render Profile info on Card component, assign a key'
-
+        return (
+          <div>
+            <button> { btnName } </button>
+            <div> { information } </div>
+          </div>
+        );
       case 'appointments' : 
         url = 'url for get all appointments'
         // this.props.fetchAppointments(url)
         btnName = 'Add Appointment'
         information = 'map over Appointment and return Card component, spread in return info and assign a key'
-
+        return (
+          <div>
+            <button> { btnName } </button>
+            <div> { information } </div>
+          </div>
+        );
       case 'providers' : 
         url = 'url for get all providers'
         // this.props.fetchProviders(url)
         btnName = 'Add Provider'
         information = 'map over providers and return Card component, spread in return info and assign a key'
-
+        return (
+          <div>
+            <button> { btnName } </button>
+            <div> { information } </div>
+          </div>
+        );
       case 'insurance' : 
         url = 'url for get all insurance'
         // this.props.fetchInsurance(url)
         btnName = 'Add Insurance'
         information = 'map over insurance and return Card component, spread in return info and assign a key'
-      
-      return(
-        <div>
-          <button> { btnName } </button>
-          <div> { information } </div>
-        </div>
-      )
+        return (
+          <div>
+            <button> { btnName } </button>
+            <div> { information } </div>
+          </div>
+        );
+      default:
+        return ('should not hit default')
     }
   }
 }
