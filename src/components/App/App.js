9050f3a19f95d37b791a6121ef3fe367484af8f0
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-14 16:50:22 -0700

    Creates form for adding profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,6 @@ import Display from '../../containers/Display/Display';
 
 
 class App extends Component {
-// <i class="fas fa-user-md"></i>
-// <i class="far fa-address-card"></i>
-// <i class="far fa-calendar-alt"></i>
-// <i class="fas fa-user-circle"></i>
-// <i class="fas fa-trash-alt"></i>
-// <i class="fas fa-edit"></i>
-// <i class="far fa-plus-square"></i>
-
 
   render() {
     return (
--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../index.scss';
 import PropTypes from 'prop-types';
+import { addAppointment } from '../../actions/index';
+import { addProvider } from '../../actions/index';
+import { addInsurance } from '../../actions/index';
+import { addProfile } from '../../actions/index';
+import { connect } from 'react-redux';
 
+class Schedule extends Component {
+  constructor() {
+    super()
+    this.state = {
+      blood: '',
+      height: '',
+      weight: '',
+    }
+  }
 
-const Schedule = (props) => {
-  return(
-    <section>
-      <form>
-        form goes here
-      </form>
-    </section>
-  )
+  render() {
+    return(
+      <section>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          this.props.handleProfileSubmit(this.state)}
+        }>
+          <input placeholder='Blood Type' value={this.state.blood} onChange={(event) => this.setState({blood: event.target.value})}/>
+          <input placeholder='Height' value={this.state.height} onChange={(event) => this.setState({height: event.target.value})}/>
+          <input placeholder='Weight' value={this.state.weight} onChange={(event) => this.setState({weight: event.target.value})}/>
+          <button>Submit</button>
+        </form>
+      </section>
+    )
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    appointment: state.appointmentReducer,
+    provider: state.providerReducer,
+    insurance: state.insuranceReducer,
+    profile: state.profileReducer,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // handleAppointmentSubmit: (text => {
+    //   dispatch(addAppointment(text))
+    // }),
+    // handleProviderSubmit: (text => {
+    //   dispatch(addProvider(text))
+    // }),
+    // handleInsuranceSubmit: (text => {
+    //   dispatch(addInsurance(text))
+    // }),
+    handleProfileSubmit: (text => {
+      dispatch(addProfile(text))
+    }),
+  }
+}
+
 
 Schedule.propTypes = {
   type: PropTypes.string
 }
 
-export default Schedule;
+export default connect(mapStateToProps, mapDispatchToProps)(Schedule);
