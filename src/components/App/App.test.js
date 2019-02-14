001c66d1ca8c76bcd8dc6f41b11ea480496adad3
Sara Ebbers <slebbers@gmail.com>
2019-02-14 11:40:02 -0700

    Add snapshot to App.js and Schedule.js, Add basic shell of Schedule to run test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
+describe('App Component', () => {
+  it('should match the screenshot', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper).toMatchSnapshot()
 
+  })
+})
