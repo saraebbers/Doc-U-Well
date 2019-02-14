fc6a6f73d468ce8088c059c246450102f76e2667
Sara Ebbers <slebbers@gmail.com>
2019-02-14 12:40:19 -0700

    Add action creators for appointment provider insurance and profile

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,23 @@ export const isLoading = (bool) => ({
   type: 'IS_LOADING',
   isLoading: bool
 })
+
+export const addAppointment = (appontmentDetails) => ({
+  type: 'ADD_APPOINTMENT',
+  appontmentDetails
+})
+
+export const addProvider = (providerDetails) => ({
+  type: 'ADD_PROVIDER',
+  providerDetails
+})
+
+export const addInsurance = (insuranceDetails) => ({
+  type: 'ADD_INSURANCE',
+  insuranceDetails
+})
+
+export const addProfile = (profileDetails) => ({
+  type: 'ADD_Profile',
+  profileDetails
+})
