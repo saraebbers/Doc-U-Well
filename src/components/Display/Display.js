f13d33e1de54b73637535a978304767d52475e2f
Sara Ebbers <slebbers@gmail.com>
2019-02-13 17:47:39 -0700

    Refactor Switch by adding function for return value, complete styling for display

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 
 class Display extends Component {
 
+  returnJsx (btnName, information) {
+    return (
+      <div className='display-container'>
+        <button className='add-btn'> { btnName } <i className="far fa-plus-square"></i></button>
+        <div className='card-area'> { information } </div>
+      </div>
+    )
+  }
 
   render() {
     let url
     let btnName
     let information
-
-    console.log(this.props)
 
     switch (this.props.type) {
       case 'profile' : 
@@ -18,45 +24,29 @@ class Display extends Component {
         // this.props.fetchProfile(url)
         btnName = 'Update Profile'
         information = 'render Profile info on Card component, assign a key'
-        return (
-          <div>
-            <button> { btnName } </button>
-            <div> { information } </div>
-          </div>
-        );
+        return this.returnJsx(btnName, information)
+
       case 'appointments' : 
         url = 'url for get all appointments'
         // this.props.fetchAppointments(url)
         btnName = 'Add Appointment'
         information = 'map over Appointment and return Card component, spread in return info and assign a key'
-        return (
-          <div>
-            <button> { btnName } </button>
-            <div> { information } </div>
-          </div>
-        );
+        return this.returnJsx(btnName, information)
+
       case 'providers' : 
         url = 'url for get all providers'
         // this.props.fetchProviders(url)
         btnName = 'Add Provider'
         information = 'map over providers and return Card component, spread in return info and assign a key'
-        return (
-          <div>
-            <button> { btnName } </button>
-            <div> { information } </div>
-          </div>
-        );
+        return this.returnJsx(btnName, information)
+
       case 'insurance' : 
         url = 'url for get all insurance'
         // this.props.fetchInsurance(url)
         btnName = 'Add Insurance'
         information = 'map over insurance and return Card component, spread in return info and assign a key'
-        return (
-          <div>
-            <button> { btnName } </button>
-            <div> { information } </div>
-          </div>
-        );
+        return this.returnJsx(btnName, information)
+
       default:
         return ('should not hit default')
     }
