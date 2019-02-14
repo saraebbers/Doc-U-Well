a15fda900e94299d18ec78f2a02e0d15c8415ed2
Sara Ebbers <slebbers@gmail.com>
2019-02-14 11:56:50 -0700

    Add action tests for has errored and isloading

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -5,4 +5,24 @@ describe('actions', () => {
 
   })
 
+  it('hasErrored should take in the payload and return an object with a type of HAS_ERRORED', () => {
+    const errorMessage = ''
+    const expected = {
+      type: 'HAS_ERRORED',
+      errorMessage
+    }
+    const result = actions.hasErrored(errorMessage)
+    expect(result).toEqual(expected)
+  })
+
+  it('isLoading should take in the payload and return an object with a type of IS_LOADING', () => {
+    const bool = true
+    const expected = {
+      type: 'IS_LOADING',
+      isLoading: true
+    }
+    const result = actions.isLoading(bool)
+    expect(result).toEqual(expected)
+  })
+
 })
