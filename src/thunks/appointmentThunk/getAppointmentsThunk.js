686313e09a31f67b9567d327c7125696ffdcb2dc
Sara Ebbers <slebbers@gmail.com>
2019-02-17 11:40:14 -0700

    Add get AppointmentsThunk

--- src/thunks/appointmentThunk/getAppointmentsThunk.js
+++ src/thunks/appointmentThunk/getAppointmentsThunk.js
@@ -3,5 +3,23 @@ import { apiKey } from '../utils/apiKey';
 
 
 export const getAppointmentsThunk = (url) => {
-  
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'api_key': `${apiKey}`
+        }
+      })
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      dispatch(isLoading(false))
+      const appointmentDetails = await response.json()
+      dispatch(addAppointments(appointmentDetails))
+    } catch(error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
 }
