27aac886f2265a83c89d4810087a4a7af1bd4398
Sara Ebbers <slebbers@gmail.com>
2019-02-18 10:56:21 -0700

    Add Profile Thunk

--- src/thunks/providerThunk/getProvidersThunk.js
+++ src/thunks/providerThunk/getProvidersThunk.js
@@ -18,7 +18,6 @@ export const getAllProvidersThunk = (url) => {
       }
       dispatch(isLoading(false))
       const providerDetails = await response.json()
-      console.log(providerDetails)
       dispatch(getProviders(providerDetails.results))
     } catch(error) {
       dispatch(hasErrored(error.message))
