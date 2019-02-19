68845ef864957e249bcd4f58f14d09499a685bcf
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-19 14:26:39 -0700

    adds initial form for login

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,11 +4,44 @@ import PropTypes from 'prop-types';
 
 
 class Home extends Component {
+  constructor(){
+    super()
+    this.state={
+      email: '',
+      password: ''
+    }
+  }
+
+  async handleSubmit() {
+    const { email, password } = this.state
+    const newUser = {...this.state}
+    await this.handleNewUser(newUser)
+  }
+
+  handleNewUser(newUser){
+    this.setState({
+      email: '',
+      password: ''
+    })
+  }
 
   render() {
+    const { email, password } = this.state
     return(
       <div className="home-container">
-        <div className="welcome-message">
+        <div className="login">
+          <form onSubmit={(event) => {
+            event.preventDefault()
+            this.handleSubmit()}}>
+            <h2>Sign Up</h2>
+            Enter e-mail: <input type='email' placeholder='email' value={email} onChange={(event) => {
+              this.setState({email:event.target.value})
+            }}/><br/>
+            Create password: <input type='password' placeholder='password' value={password} onChange={(event) => {
+              this.setState({password:event.target.value})
+            }}/><br/>
+            <button>Submit</button>
+          </form>
         </div>
       </div>
     )
