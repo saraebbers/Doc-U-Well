fdaafce3f24a7d14ea4c874df56f1a9c8b85d141
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-20 16:48:00 -0700

    add functionality to post a profile

---
+++ src/thunks/profileThunk/postProfileThunk.js
@@ -0,0 +1,28 @@
+import { isLoading, hasErrored, addProfile } from '../../actions/index'
+
+export const addProfileThunk = (user, payload) => {
+
+  let url = 'https://my-health-tracker.herokuapp.com/api/v1/profiles'
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload)
+      })
+      if(!response.ok) {
+        throw Error(response.statusText)
+        console.log('error', response.statusText)
+      }
+      dispatch(isLoading(false))
+      const profileDetails = await response.json()
+      dispatch(addProfile(profileDetails.data))
+    } catch(error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
+}
